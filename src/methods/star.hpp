75f1d84718c60057b7a3240aa0f32a8023a29aff
Karolis Zukauskas <Karolis.Zukauskas@bentley.com>
2021-01-06 00:31:35 +0200

    Bring back corrupted (deleted) files ...

---
+++ src/methods/star.hpp
@@ -0,0 +1,168 @@
+/***************************************************************************
+ *                                                                          *
+ *   Copyright (C) 2018 by David B. Blumenthal                              *
+ *                                                                          *
+ *   This file is part of GEDLIB.                                           *
+ *                                                                          *
+ *   GEDLIB is free software: you can redistribute it and/or modify it      *
+ *   under the terms of the GNU Lesser General Public License as published  *
+ *   by the Free Software Foundation, either version 3 of the License, or   *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   GEDLIB is distributed in the hope that it will be useful,              *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the           *
+ *   GNU Lesser General Public License for more details.                    *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with GEDLIB. If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                          *
+ ***************************************************************************/
+
+/*!
+ * @file  star.hpp
+ * @brief ged::Star class declaration.
+ */
+
+#ifndef SRC_METHODS_STAR_HPP_
+#define SRC_METHODS_STAR_HPP_
+
+namespace ged {
+
+/*!
+ * @brief Computes lower and upper bounds for uniform edit costs.
+ * @details Implements the method Star suggested in:
+ * - Z. Zeng, A. K. H. Tung, J. Wang, J. Feng, and L. Zhou:
+ *   &ldquo;Comparing stars: On approximating graph edit distance&rdquo;,
+ *   https://doi.org/10.1016/10.14778/1687627.1687631
+ *
+ * Supports the following option in addition to the ones supported by ged::LSAPEBasedMethod:
+ * | <tt>\--@<option@> @<arg@></tt> | modified parameter | default  | more information |
+ * | ------------------------------ | ------------------ | -------- | ---------------- |
+ * | <tt>\--sort-method STD\|COUNTING</tt> | the employed sorting algorithm | @p COUNTING | @ref ged::util::counting_sort() <br> use counting sort if the number of different edge labels is constant |
+ */
+template<class UserNodeLabel, class UserEdgeLabel>
+class Star : public LSAPEBasedMethod<UserNodeLabel, UserEdgeLabel> {
+
+public:
+
+	virtual ~Star();
+
+	Star(const GEDData<UserNodeLabel, UserEdgeLabel> & ged_data);
+
+public:
+
+	enum SortMethod_ {STD, COUNTING};
+
+	class SortedNodeLabels_ {
+	public:
+		SortedNodeLabels_(const GEDGraph & g, SortMethod_ sort_method);
+
+		SortedNodeLabels_();
+
+		void operator=(const SortedNodeLabels_ & sorted_edge_labels);
+
+		const std::vector<LabelID> & get_incident_labels(GEDGraph::NodeID) const;
+
+	private:
+		std::map<GEDGraph::NodeID, std::vector<LabelID>> sorted_node_labels_;
+	};
+
+	SortMethod_ sort_method_;
+
+	std::map<GEDGraph::GraphID, SortedNodeLabels_> sorted_node_labels_;
+
+	// Inherited member functions from LSAPEBasedMethod.
+
+	virtual void lsape_populate_instance_(const GEDGraph & g, const GEDGraph & h, DMatrix & lsape_instance);
+
+	virtual bool lsape_parse_option_(const std::string & option, const std::string & arg) final;
+
+	virtual std::string lsape_valid_options_string_() const final;
+
+	virtual void lsape_set_default_options_() final;
+
+	virtual void lsape_init_graph_(const GEDGraph & graph) final;
+
+	virtual double lsape_lower_bound_scaling_factor_(const GEDGraph & g, const GEDGraph & h) final;
+
+	// Helper member functions.
+
+	double compute_substitution_cost_(const GEDGraph & g, const GEDGraph & h, GEDGraph::NodeID i, GEDGraph::NodeID k,
+			const SortedNodeLabels_ & sorted_node_labels_g, const SortedNodeLabels_ & sorted_node_labels_h) const;
+
+	double compute_deletion_cost_(const GEDGraph & g, GEDGraph::NodeID i) const;
+
+	double compute_insertion_cost_(const GEDGraph & h, GEDGraph::NodeID k) const;
+};
+
+/**
+ * Modifications:
+ * Insertion and deletion cost functions are taken from BIPARTINE/BRANCH.
+ */
+template<class UserNodeLabel, class UserEdgeLabel>
+class Star2 : public Star<UserNodeLabel, UserEdgeLabel> {
+public:
+  Star2(const GEDData<UserNodeLabel, UserEdgeLabel> & ged_data);
+
+  double compute_deletion_cost_(const GEDGraph & g, GEDGraph::NodeID i) const;
+  double compute_insertion_cost_(const GEDGraph & h, GEDGraph::NodeID k) const;
+};
+
+/**
+ * Modifications:
+ * Insertion and deletion cost functions are taken from BIPARTINE/BRANCH and they are not multiplied by minimum edit costs
+ * in lsape_populate_instance_().
+ */
+template<class UserNodeLabel, class UserEdgeLabel>
+class Star3 : public Star<UserNodeLabel, UserEdgeLabel> {
+public:
+  Star3(const GEDData<UserNodeLabel, UserEdgeLabel> & ged_data);
+
+  virtual void lsape_populate_instance_(const GEDGraph & g, const GEDGraph & h, DMatrix & lsape_instance);
+  double compute_deletion_cost_(const GEDGraph & g, GEDGraph::NodeID i) const;
+  double compute_insertion_cost_(const GEDGraph & h, GEDGraph::NodeID k) const;
+};
+
+/**
+ * Modifications:
+ * Mean node edit consts are used instead of minimum costs in lsape_populate_instance_
+ */
+template<class UserNodeLabel, class UserEdgeLabel>
+class Star4 : public Star<UserNodeLabel, UserEdgeLabel> {
+public:
+  Star4(const GEDData<UserNodeLabel, UserEdgeLabel> & ged_data);
+
+  virtual void lsape_populate_instance_(const GEDGraph & g, const GEDGraph & h, DMatrix & lsape_instance);
+};
+
+/**
+ * Modifications:
+ * 1) Mean node edit consts are used instead of minimum costs in lsape_populate_instance_.
+ * 2) If the matrix is square, node insert/delete consts are set to infinity.
+ */
+template<class UserNodeLabel, class UserEdgeLabel>
+class Star5 : public Star<UserNodeLabel, UserEdgeLabel> {
+public:
+  Star5(const GEDData<UserNodeLabel, UserEdgeLabel> & ged_data);
+
+  virtual void lsape_populate_instance_(const GEDGraph & g, const GEDGraph & h, DMatrix & lsape_instance);
+};
+
+/**
+ * Modifications:
+ * 1) Mean node edit consts are used instead of minimum costs in lsape_populate_instance_.
+ * 2) If the matrix is "almost square", node insert/delete consts are set to infinity. "Almost equal" is calculuted as
+ * `abs(n - m) <= threshold` where threshold is calculated as `round(min(n, m) / 10)`.
+ */
+template<class UserNodeLabel, class UserEdgeLabel>
+class Star6 : public Star<UserNodeLabel, UserEdgeLabel> {
+public:
+  Star6(const GEDData<UserNodeLabel, UserEdgeLabel> & ged_data);
+
+  virtual void lsape_populate_instance_(const GEDGraph & g, const GEDGraph & h, DMatrix & lsape_instance);
+};
+
+}
+
+#endif /* SRC_METHODS_STAR_HPP_ */
---
+++ tests/kazu/src/main.cpp
@@ -0,0 +1,404 @@
+/***************************************************************************
+ *                                                                          *
+ *   Copyright (C) 2018 by David B. Blumenthal                              *
+ *                                                                          *
+ *   This file is part of GEDLIB.                                           *
+ *                                                                          *
+ *   GEDLIB is free software: you can redistribute it and/or modify it      *
+ *   under the terms of the GNU Lesser General Public License as published  *
+ *   by the Free Software Foundation, either version 3 of the License, or   *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   GEDLIB is distributed in the hope that it will be useful,              *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the           *
+ *   GNU Lesser General Public License for more details.                    *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with GEDLIB. If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                          *
+ ***************************************************************************/
+
+/*!
+ * @file tests/vldbj2019/src/test_best_methods.cpp
+ * @brief Runs tests for VLDB J. submission.
+ * @details The binary built from this file was used for the experiments in the following paper:
+ * - D. B. Blumenthal, N. Boria, J. Gamper, S. Bougleux L. Brun:
+ *   &ldquo;Comparing heuristics for graph edit distance computation&rdquo;,
+ *   VLDB J. 2019
+ */
+
+#include "util.hpp"
+#include "graph_gen.hpp"
+#include <functional>
+
+using GxlGEDEnv = ged::GEDEnv<ged::GXLNodeID, ged::GXLLabel, ged::GXLLabel>;
+
+constexpr int TEST_THREADS = 2;
+
+class Method {
+private:
+  ged::Options::GEDMethod ged_method_;
+  std::string initialization_method_;
+  bool use_optimal_lsape_initialization_;
+  std::string extra_options_;
+
+  std::string options_() const {
+    // TODO: Do we want to run algorithms multithreaded?
+    std::string options("--threads 1");
+
+    if (initialization_method_ == "REFINE") {
+      options += " --ls-initialization-method " + initialization_method_;
+    } else {
+      //options += std::string(" --optimal_initialization ") + (use_optimal_lsape_initialization_ ? "TRUE" : "FALSE");
+      options += " --initialization-method " + initialization_method_;
+    }
+
+    return options + extra_options_;
+  }
+
+public:
+  Method(ged::Options::GEDMethod ged_method, std::string const& initialization_method, bool use_optimal_lsape_initialization, std::string const& extra_options = "")
+    : ged_method_{ged_method},
+      initialization_method_{initialization_method},
+      use_optimal_lsape_initialization_{use_optimal_lsape_initialization},
+      extra_options_(extra_options) {}
+
+    std::string name() const {
+      std::stringstream name;
+
+      if (ged_method_ == ged::Options::GEDMethod::IPFP) {
+        name << "IPFP";
+
+        // TEMP
+        if (!extra_options_.empty())
+          name << " (" << extra_options_ << ")";
+      } else if (ged_method_ == ged::Options::GEDMethod::REFINE) {
+        name << "REFINE";
+      } else {
+        assert(false);
+      }
+
+      name << " (" << initialization_method_ << ")";
+
+      return name.str();
+    }
+
+    void run_on_dataset(const std::string & dataset, GxlGEDEnv & env, bool ensure_n_greater_m,
+      double & avg_lb, double & avg_ub, double & avg_runtime, double& avg_init_time, double& avg_ls_iterations_time, double& avg_num_ls_iterations) const {
+
+      env.set_method(ged_method_, options_());
+      if (dataset != "Protein" or ged_method_ != ged::Options::GEDMethod::PARTITION) {
+        env.init_method();
+      }
+
+      std::size_t num_runs{env.graph_ids().second * env.graph_ids().second};
+      ged::ProgressBar progress_bar(num_runs);
+
+      avg_runtime = 0;
+      avg_ub = 0;
+      avg_lb = 0;
+      avg_init_time = 0;
+      avg_ls_iterations_time = 0;
+      avg_num_ls_iterations = 0;
+
+      //std::cout << "\n\tDataset: " << dataset << "   Method: " << name() << std::endl;
+
+      for (ged::GEDGraph::GraphID g_id = env.graph_ids().first; g_id != env.graph_ids().second; g_id++) {
+        for (ged::GEDGraph::GraphID h_id = env.graph_ids().first; h_id != env.graph_ids().second; h_id++) {
+          ged::GEDGraph::GraphID _g_id = g_id;
+          ged::GEDGraph::GraphID _h_id = h_id;
+
+          // Some greedy LSAPE solvers require graph nodes to be n >= m
+          if (ensure_n_greater_m && env.get_num_nodes(_g_id) < env.get_num_nodes(_h_id))
+            std::swap(_g_id, _h_id);
+
+          env.run_method(_g_id, _h_id);
+
+          avg_lb += env.get_lower_bound(_g_id, _h_id);
+          avg_ub += env.get_upper_bound(_g_id, _h_id);
+          avg_runtime += env.get_runtime(_g_id, _h_id);
+
+          auto pLSMethod = dynamic_cast<ged::LSBasedMethod<ged::GXLLabel, ged::GXLLabel>*>(env.get_method());
+          assert(pLSMethod != nullptr);
+
+          avg_init_time += pLSMethod->initial_sulutions_time.count();
+          avg_ls_iterations_time += pLSMethod->ls_iterations_time.count();
+          avg_num_ls_iterations += static_cast<double>(pLSMethod->num_ls_iterations);
+
+          progress_bar.increment();
+          //std::cout << "\r\t" << name() << ": " << progress_bar << std::flush;
+        }
+      }
+
+      avg_lb /= static_cast<double>(num_runs);
+      avg_ub /= static_cast<double>(num_runs);
+      avg_runtime /= static_cast<double>(num_runs);
+      avg_init_time /= static_cast<double>(num_runs);
+      avg_ls_iterations_time /= static_cast<double>(num_runs);
+      avg_num_ls_iterations /= static_cast<double>(num_runs);
+
+      // std::cout << "\n";
+    }
+};
+
+std::string create_result_file(std::string const& base_name, std::string const& dataset) {
+  std::time_t t = std::time(0);
+  std::tm* now = std::localtime(&t);
+  std::stringstream ss;
+  ss << "../results/" << base_name << "_" << dataset << "_" << now->tm_mon << "-" << now->tm_mday << "_"
+    << now->tm_hour << "-" << now->tm_min << "-" << now->tm_sec << ".csv";
+
+  std::string filename = ss.str();
+  std::ofstream result_file(filename.c_str());
+
+  result_file << "method, num_nodes, edge_density, lb, ub, runtime, ls_iterations, init_solutions_t, ls_iterations_t" << std::endl;
+  result_file.close();
+
+  return filename;
+}
+
+void run_methods(std::vector<Method> const& methods, std::function<GxlGEDEnv()> const& setup_ged_env, std::string const& dataset,
+  double edge_density, bool ensure_n_greater_m, std::string const& result_filename) {
+
+#ifdef _OPENMP
+    omp_set_num_threads(TEST_THREADS);
+#pragma omp parallel for if(TEST_THREADS > 1) schedule(dynamic)
+#endif
+  for (std::size_t i = 0; i < methods.size(); ++i) {
+    double avg_ub{0};
+    double avg_lb{0};
+    double avg_runtime{0};
+    double avg_init_time{0};
+    double avg_ls_iterations_time{0};
+    double avg_num_ls_iterations = 0;
+
+    // GEDEnv is tightly coupled with it's graph_data and edit_costs data structures - can't reuse that memory without making a lot of
+    // adjustments to the whole gedlib. Initialize a copy of GEDEnv (includes graph and edit cost data) for each run of a method (needed for parallelization).
+    GxlGEDEnv env = setup_ged_env();
+
+    methods[i].run_on_dataset(dataset, env, ensure_n_greater_m, avg_lb, avg_ub, avg_runtime, avg_init_time, avg_ls_iterations_time, avg_num_ls_iterations);
+
+#pragma omp critical
+    {
+      std::ofstream result_file(result_filename.c_str(), std::ios_base::app);
+
+      result_file << methods[i].name() << "," << env.get_avg_num_nodes() << "," << edge_density << ",";
+      result_file << avg_lb << "," << avg_ub << "," << avg_runtime << "," << avg_num_ls_iterations << ",";
+      result_file << avg_init_time << "," << avg_ls_iterations_time << "\n";
+      result_file.close();
+    }
+  }
+}
+
+void run_on_sized_dataset(std::vector<Method> const& methods, std::string const& dataset, bool ensure_n_greater_m, std::string const& result_filename) {
+  std::cout << "\n=== " << dataset << " ===\n";
+
+  std::size_t max_max_size_div_10 {0};
+  if (dataset == "AIDS") {
+    max_max_size_div_10 = 8;
+  }
+  else if (dataset == "Protein") {
+    // NOTE: originaly Protein is divided into 6 datasets
+    max_max_size_div_10 = 7;
+  }
+  else if (dataset == "Mutagenicity") {
+    max_max_size_div_10 = 10;
+  }
+
+  for (std::size_t max_size_dev_10{1}; max_size_dev_10 <= max_max_size_div_10; max_size_dev_10++) {
+    std::cout << "\n@@@ Dataset Partition: " << max_size_dev_10 << "\n";
+
+    auto setup_ged_env = [&dataset, &max_size_dev_10]() -> GxlGEDEnv {
+      GxlGEDEnv env;
+      util::setup_environment(dataset, max_size_dev_10, env);
+
+      return env;
+    };
+
+    run_methods(methods, setup_ged_env, dataset, 0.0, ensure_n_greater_m, result_filename);
+  }
+}
+
+void run_on_test_dataset(std::vector<Method> const& methods, std::string const& dataset, bool ensure_n_greater_m, std::string const& result_filename) {
+  std::cout << "\n=== " << dataset << " ===\n";
+
+  auto setup_ged_env = [&dataset]() -> GxlGEDEnv {
+    GxlGEDEnv env;
+    util::setup_environment(dataset, false, env);
+
+    return env;
+  };
+
+  run_methods(methods, setup_ged_env, dataset, 0.0, ensure_n_greater_m, result_filename);
+}
+
+void run_on_generated_dataset(std::vector<Method> const& methods, std::function<void(GxlGEDEnv&)> const& generate_graphs, std::string const& dataset,
+  double edge_density, bool ensure_n_greater_m, std::string const& result_filename) {
+  std::cout << "\n=== " << dataset << " ===\n";
+
+  auto setup_ged_env = [&generate_graphs]() -> GxlGEDEnv {
+    GxlGEDEnv env;
+    generate_graphs(env);
+
+    return env;
+  };
+
+  run_methods(methods, setup_ged_env, dataset, edge_density, ensure_n_greater_m, result_filename);
+}
+
+void test_ls_all_datasets() {
+  std::vector<std::string> datasets = {
+    "Letter_HIGH", "Mutagenicity", "AIDS", "Protein", "GREC", "Fingerprint",
+  };
+
+  std::vector<Method> const methods {
+    Method (ged::Options::GEDMethod::IPFP, "REFINE", true, " --initialization-method BRANCH_UNIFORM"),
+    Method (ged::Options::GEDMethod::IPFP, "REFINE", true, " --initialization-method STAR"),
+
+    Method (ged::Options::GEDMethod::IPFP, "STAR", true),
+    Method (ged::Options::GEDMethod::IPFP, "STAR3", true),
+    Method (ged::Options::GEDMethod::IPFP, "STAR4", true),
+    Method (ged::Options::GEDMethod::IPFP, "BRANCH_UNIFORM", true),
+    Method (ged::Options::GEDMethod::IPFP, "BRANCH_UNIFORM2", true),
+    // Method (ged::Options::GEDMethod::IPFP, "BRANCH_FAST", true),
+    // Method (ged::Options::GEDMethod::IPFP, "BRANCH", true),
+    // Method (ged::Options::GEDMethod::IPFP, "NODE", true),
+    // Method (ged::Options::GEDMethod::IPFP, "BIPARTITE", true),
+    // Method (ged::Options::GEDMethod::IPFP, "RANDOM", true),
+
+    // Method (ged::Options::GEDMethod::REFINE, "STAR", true),
+    // Method (ged::Options::GEDMethod::REFINE, "BRANCH_UNIFORM", true),
+    // Method (ged::Options::GEDMethod::REFINE, "BRANCH_FAST", true),
+    // Method (ged::Options::GEDMethod::REFINE, "BRANCH", true),
+    // Method (ged::Options::GEDMethod::REFINE, "NODE", true),
+    // Method (ged::Options::GEDMethod::REFINE, "BIPARTITE", true),
+    // Method (ged::Options::GEDMethod::REFINE, "RANDOM", true),
+  };
+
+  for (auto dataset : datasets) {
+    try {
+      std::string results_filename = create_result_file("ls_all_datasets", dataset);
+
+      run_on_test_dataset(methods, dataset, false, results_filename);
+    }
+    catch (const std::exception & error) {
+      std::cerr << error.what() << ". " << "Error on test_ls_all_datasets: " << dataset << ".\n";
+    }
+  }
+}
+
+void test_ls_graph_sizes() {
+  std::vector<std::string> datasets = {
+    "AIDS", "Protein", "Mutagenicity",
+  };
+
+  std::vector<Method> const methods {
+    Method (ged::Options::GEDMethod::IPFP, "STAR", true),
+    Method (ged::Options::GEDMethod::IPFP, "STAR3", true),
+    Method (ged::Options::GEDMethod::IPFP, "STAR4", true),
+    Method (ged::Options::GEDMethod::IPFP, "BRANCH_UNIFORM", true),
+    Method (ged::Options::GEDMethod::IPFP, "BRANCH_UNIFORM2", true),
+    Method (ged::Options::GEDMethod::IPFP, "REFINE", true, " --initialization-method BRANCH_UNIFORM"),
+    Method (ged::Options::GEDMethod::IPFP, "REFINE", true, " --initialization-method STAR"),
+
+    // Method (ged::Options::GEDMethod::IPFP, "STAR", true),
+    // Method (ged::Options::GEDMethod::IPFP, "STAR3", true),
+    // Method (ged::Options::GEDMethod::IPFP, "STAR4", true),
+    // Method (ged::Options::GEDMethod::IPFP, "BIPARTITE", true),
+    // Method (ged::Options::GEDMethod::IPFP, "BRANCH_UNIFORM", true),
+    // Method (ged::Options::GEDMethod::IPFP, "BRANCH_FAST", true),
+    // Method (ged::Options::GEDMethod::IPFP, "BRANCH", true),
+    // Method (ged::Options::GEDMethod::IPFP, "NODE", true),
+    // Method (ged::Options::GEDMethod::IPFP, "RANDOM", true),
+  };
+
+  for (auto dataset : datasets) {
+    try {
+      std::string results_filename = create_result_file("ls_graph_sizes", dataset);
+
+      run_on_sized_dataset(methods, dataset, false, results_filename);
+    }
+    catch (const std::exception & error) {
+      std::cerr << error.what() << ". " << "Error on test_ls_graph_sizes: " << dataset << ".\n";
+    }
+  }
+}
+
+void test_ls_rand_graphs() {
+  std::vector<Method> const methods {
+    Method (ged::Options::GEDMethod::IPFP, "STAR5", true),
+    Method (ged::Options::GEDMethod::IPFP, "STAR6", true),
+
+    // Method (ged::Options::GEDMethod::IPFP, "STAR", true),
+    // Method (ged::Options::GEDMethod::IPFP, "STAR3", true),
+    // Method (ged::Options::GEDMethod::IPFP, "STAR4", true),
+    // Method (ged::Options::GEDMethod::IPFP, "BIPARTITE", true),
+    // Method (ged::Options::GEDMethod::IPFP, "BRANCH_UNIFORM", true),
+    // Method (ged::Options::GEDMethod::IPFP, "BRANCH_UNIFORM2", true),
+    // Method (ged::Options::GEDMethod::IPFP, "BRANCH_FAST", true),
+    // Method (ged::Options::GEDMethod::IPFP, "BRANCH", true),
+    // Method (ged::Options::GEDMethod::IPFP, "NODE", true),
+    // Method (ged::Options::GEDMethod::IPFP, "RANDOM", true),
+    // Method (ged::Options::GEDMethod::IPFP, "REFINE", true, " --initialization-method BRANCH_UNIFORM"),
+    // Method (ged::Options::GEDMethod::IPFP, "REFINE", true, " --initialization-method STAR"),
+    // Method (ged::Options::GEDMethod::IPFP, "REFINE", true, " --initialization-method NODE"),
+    // Method (ged::Options::GEDMethod::IPFP, "REFINE", true, " --initialization-method BIPARTITE"),
+  };
+
+  size_t const num_graphs = 100;
+  size_t const node_variance = 5;
+  std::vector<size_t> const graph_sizes {
+    10, 20, 30, // 10, 20, 30, 40, 50,
+  };
+  std::vector<double> const edge_densities {
+    0.1, 0.2, 0.3, // 0.4, 0.5, 0.6,
+  };
+  std::vector<std::string> const node_labels { "A", "B", "C" };
+  std::vector<std::string> const edge_labels { "0", "1" };
+
+  std::string results_filename = create_result_file("ls_generated", "rand");
+  for (size_t graph_size : graph_sizes) {
+    for (double edge_density : edge_densities) {
+      std::stringstream ss;
+      ss << "random, nodes: " << graph_size << " edge density: " << edge_density;
+      std::string dataset = ss.str();
+
+      try {
+        auto generate_graphs = [num_graphs, graph_size, edge_density, &node_labels, &edge_labels, node_variance](GxlGEDEnv& env) -> void {
+          std::mt19937 size_variance_rng(0);          // For graph_size variance
+          std::mt19937 graph_gen_rng(42);             // For generating graphs
+
+          for (size_t i = 0; i < num_graphs; ++i) {
+            double multiplier = static_cast<double>((static_cast<long>(size_variance_rng() % 2000)) - 1000) / 1000.0;
+            size_t num_nodes = graph_size + static_cast<size_t>(std::round(multiplier * static_cast<double>(node_variance)));
+
+            ged::graph_gen_random(env, graph_gen_rng, num_nodes, edge_density, node_labels, edge_labels);
+          }
+
+          util::setup_rand_environment(env);
+        };
+
+        run_on_generated_dataset(methods, generate_graphs, dataset, edge_density, false, results_filename);
+      } catch (const std::exception & error) {
+        std::cerr << error.what() << ". " << "Error on test_ls_rand_graphs: " << dataset << ".\n";
+      }
+    }
+  }
+}
+
+void test_greedy_lsape() {
+
+}
+
+void test_iterations() {
+
+}
+
+int main(int argc, char* argv[]) {
+  test_ls_rand_graphs();
+  //test_ls_all_datasets();
+  //test_ls_graph_sizes();
+
+  return 0;
+}
